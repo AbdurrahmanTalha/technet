fbc3d623013ac37f12ec3d49b54373fcf4cd703e
AbdurrahmanTalha <abdurrahmantalha.dev@gmail.com>
2023-07-13 20:06:06 +0600

    e

---
+++ src/redux/features/products/productApi.ts
@@ -0,0 +1,8 @@
+import { api } from '@/redux/api/apiSlice';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const productApi = api.injectEndpoints({
+    endpoints: (builder) => ({
+        
+    })
+})
